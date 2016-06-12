b72fa5f31df474e7b53331d6fb2e23a484c61304
Le Hoang Quyen <lehoangquyen@Les-Mac.local>
2016-06-13 00:03:22 +0800

    - support interfaces addresses listing for unix

--- unix/ConnectionHandlerUnix.cpp
+++ unix/ConnectionHandlerUnix.cpp
@@ -1,6 +1,7 @@
 #include "../ConnectionHandler.h"
 
 #include <fcntl.h>
+#include <ifaddrs.h>
 
 namespace HQRemote {
 	int SocketConnectionHandler::platformSetSocketBlockingMode(socket_t socket, bool blocking)
@@ -21,11 +22,32 @@ namespace HQRemote {
 	}
 
 	void SocketServerHandler::platformGetLocalAddressesForMulticast(std::vector<struct in_addr>& addresses) {
-		//default address
 		addresses.clear();
+#if 1
+		struct ifaddrs *ifap_buf;
+		if (getifaddrs(&ifap_buf) == 0)
+		{
+			auto ifap = ifap_buf;
+			while (ifap)
+			{
+				if (ifap->ifa_addr->sa_family == AF_INET)
+				{
+					auto& addr = *(struct sockaddr_in*)ifap->ifa_addr;
+					addresses.push_back(addr.sin_addr);
+				}
+				ifap = ifap->ifa_next;//next interface
+			}
+			
+			freeifaddrs(ifap_buf);
+		}
+		else
+#endif
+		{
+			//default address
 
-		struct in_addr _default;
-		_default.s_addr = htonl(INADDR_ANY);
-		addresses.push_back(_default);
+			struct in_addr _default;
+			_default.s_addr = htonl(INADDR_ANY);
+			addresses.push_back(_default);
+		}
 	}
 }
